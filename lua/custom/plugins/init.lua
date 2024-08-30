e525e788f6b86e0a0447a6270953c8aa126e70b0
peterrrock2 <27579114+peterrrock2@users.noreply.github.com>
2024-08-29 23:28:07 -0600

    Add toggleterm and configure

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,4 +6,6 @@ return {
   { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
   { 'rebelot/kanagawa.nvim', name = 'kanagawa', priority = 1000 },
   { 'navarasu/onedark.nvim', name = 'onedark', priority = 1000 },
+  { 'nvim-lualine/lualine.nvim', name = 'lualine', dependencies = { 'nvim-tree/nvim-web-devicons' } },
+  { 'akinsho/toggleterm.nvim', version = '*', config = true },
 }
