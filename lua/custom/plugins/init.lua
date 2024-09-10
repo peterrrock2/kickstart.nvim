3f5e6b7159fd3734d07aa2227998cec77de8821c
peterrrock2 <peter.r.rock2@gmail.com>
2024-09-09 20:49:26 -0600

    Add buffer settings

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -8,4 +8,15 @@ return {
   { 'navarasu/onedark.nvim', name = 'onedark', priority = 1000 },
   { 'nvim-lualine/lualine.nvim', name = 'lualine', dependencies = { 'nvim-tree/nvim-web-devicons' } },
   { 'akinsho/toggleterm.nvim', version = '*', config = true },
+  { 'akinsho/bufferline.nvim', version = '*', dependencies = 'nvim-tree/nvim-web-devicons' },
+  {
+    'rmagatti/auto-session',
+    lazy = false,
+    dependencies = {
+      'nvim-telescope/telescope.nvim', -- Only needed if you want to use session lens
+    },
+    opts = {
+      suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+    },
+  },
 }
