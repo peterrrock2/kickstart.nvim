2a585adc19371e2661a9a86e34f4f8b39c12e53a
peterrrock2 <27579114+peterrrock2@users.noreply.github.com>
2025-08-14 21:27:53 -0600

    Add bash formatter

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -32,11 +32,21 @@ return { -- Autoformat
       lua = { 'stylua' },
       python = { 'black' },
       rust = { 'rustfmt' },
+      sh = { 'shfmt' },
+      bash = { 'shfmt' },
+      markdown = { 'mdformat' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
+    },
+    formatters = {
+      shfmt = {
+        -- indent 4, indent case/esac, simplify, indent binops, keep columns
+        prepend_args = { '-i', '4', '-ci', '-sr', '-bn', '-kp' },
+      },
+      mdformat = { prepend_args = { '--wrap', '100' } },
     },
   },
 }
