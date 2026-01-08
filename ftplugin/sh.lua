54e01ecaee13afe7617cf00b42241767bec90615
peterrrock2 <27579114+peterrrock2@users.noreply.github.com>
2026-01-08 07:28:21 -0700

    Add in lazygit

---
+++ ftplugin/sh.lua
@@ -0,0 +1,6 @@
+-- Make the number of space that a tab character occupies 4
+vim.o.tabstop = 4
+-- Number of spaces to use for each step of an indentation
+vim.o.shiftwidth = 4
+-- Convert tabs to spaces
+vim.o.expandtab = false
