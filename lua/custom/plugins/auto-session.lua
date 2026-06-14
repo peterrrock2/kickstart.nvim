ea8cca8884969036f5fd7aeee4474533e1b410e9
peterrrock2 <27579114+peterrrock2@users.noreply.github.com>
2026-06-13 21:05:55 -0600

    auto-session doesn't save terminals

--- lua/custom/plugins/auto-session.lua
+++ lua/custom/plugins/auto-session.lua
@@ -4,6 +4,10 @@ return {
   dependencies = {
     'nvim-telescope/telescope.nvim', -- Only needed if you want to use session lens
   },
+  init = function()
+    -- Drop 'terminal' so terminal buffers are not saved/restored with the session.
+    vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,localoptions'
+  end,
   opts = {
     log_level = 'error',
     enabled = true,
